50f46cd9eade0d48d164812cc099ca9981019c9d
Nicola Salsotto <nicolasalsotto@gmail.com>
2018-12-11 13:07:26 +0100

    Use correct sed syntax

--- designer-fix.sh
+++ designer-fix.sh
@@ -11,28 +11,11 @@ if [ $(id -u) -ne 0 ]; then
 	exit 1
 fi
 
-
 #Comment out the only non-commented line in file /lib/udev/rules.d/50-bluetooth-hci-auto-poweron.rules
-fl=/lib/udev/rules.d/50-bluetooth-hci-auto-poweron.rules
-mv $fl $fl.old
-sed 's/^/#/' $fl.old > $fl
-rm -f $fl.old
-
+sed -i 's/^/#/' /lib/udev/rules.d/50-bluetooth-hci-auto-poweron.rules
 
 #Uncomment lines [Policy] and AutoEnable=true (originaly there is =false, change it) in /etc/bluetooth/main.conf
-fl=/etc/bluetooth/main.conf
-mv $fl $fl.old
-sed 's/\#\[Policy\]/\[Policy\]/g' $fl.old > $fl
-rm -f $fl.old
-
-mv $fl $fl.old
-sed 's/AutoEnable=false/AutoEnable=true/g' $fl.old > $fl
-rm -f $fl.old
-
-mv $fl $fl.old
-sed 's/\#AutoEnable=true/AutoEnable=true/g' $fl.old > $fl
-rm -f $fl.old
-
+sed -i 's/\#\[Policy\]/\[Policy\]/g; s/AutoEnable=false/AutoEnable=true/g; s/\#AutoEnable=true/AutoEnable=true/g' /etc/bluetooth/main.conf
 
 echo "Now reboot your system."
 echo "After the reboot, search and pair your mouse. If the cursor is not moving, try pairing again."
